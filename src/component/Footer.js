9a82fae68a302cebd6447498d4759070af7888b4
Manish kumar <91480902+manni2000@users.noreply.github.com>
2022-08-07 17:12:12 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <div class="footer-copyright text-center py-3"><b> Made by © Manish</b><br/>
-       <p>Github:<a href="https://github.com/manni2000/Meme-Generator"> Source code</a></p>
+       <p>Github:<a href="https://github.com/manni2000/MemesGenerator"> Source code</a></p>
       </div>
     </footer>
   );
